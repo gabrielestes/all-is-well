f7fb2ce169710c77fa795eaf44544d410d6cdb17
Marian Powell <marianpowell@gmail.com>
2016-11-15 09:27:17 -0500

    update Gabe's recent changes.

---
+++ app/assets/javascripts/therapists.index.js
@@ -0,0 +1,8 @@
+(function($){
+"use strict";
+  $(document).on('click', '.sort', function(event){
+    $(event.target).parent('.sort').find('a').toggleClass('active');
+    event.preventDefault();
+  });
+
+})(jQuery);
