a5ea5166bc7abd8bc8c602acdc09bf0cec0423e7
Marian Powell <marianpowell@gmail.com>
2016-11-16 18:25:55 -0500

    some styling done on edit t_profile. lost collapsibility though.

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -2,6 +2,7 @@
   "use strict";
   $(document).on('click','a.edit',function(event){
     $(event.target).toggleClass('active').parent().parent().next('aside').toggleClass('disappear');
+    console.log($(event.target).parent().parent().next('aside'));
     event.preventDefault();
   });
 
