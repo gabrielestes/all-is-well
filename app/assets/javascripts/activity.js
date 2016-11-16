be8343f4652313ad2fd2e928ffa62f26b968ef62
Marian Powell <marianpowell@gmail.com>
2016-11-16 10:42:17 -0500

    activity list can be filtered based on class of activity in the patient_card div

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,7 +1,6 @@
 (function($){
   $(document).on('click','.filter_title a',function(event){
-
-    console.log(event.target);
+    $('.patient_card').removeClass('hide');
     $('.filter_title a').each(function(){
       if ( $(this).hasClass('active')) {
         $(this).toggleClass('active');
@@ -9,6 +8,18 @@
     });
     $(event.target).toggleClass('active');
     event.preventDefault();
+    var classes = $(event.target).attr('class');
+    var activity = classes.split(/[_ ]/)[1];
+    $('.patient_card').each(function(){
+      if($(this).hasClass(activity)) {
+        $(this).show();
+      } else if (activity === 'all'){
+        $(this).show();
+      }
+      else {
+        $(this).hide();
+      }
+    });
   });
 
 })(jQuery);
