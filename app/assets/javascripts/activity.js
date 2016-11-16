6fa36199bbe143a24195af7778c395aaa7756aab
Marian Powell <marianpowell@gmail.com>
2016-11-16 09:30:45 -0500

    made partial for activities

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,7 +1,7 @@
 (function($){
   $(document).on('click','.filter_title a',function(event){
-    var type = $('event.target').attr();
-    console.log(type);
+
+    console.log(event.target);
     $('.filter_title a').each(function(){
       if ( $(this).hasClass('active')) {
         $(this).toggleClass('active');
