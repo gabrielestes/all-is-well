93bd3aa2e376388e2667c770209081c37902e03c
Marian Powell <marianpowell@gmail.com>
2016-11-15 21:40:30 -0500

    added footer partial. working on jQuery for activity page.

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,12 +1,14 @@
-// (function($){
-//   $('.filter_title').
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// })(JQuery);
+(function($){
+  $(document).on('click','.filter_title a',function(event){
+    var type = $('event.target').attr();
+    console.log(type);
+    $('.filter_title a').each(function(){
+      if ( $(this).hasClass('active')) {
+        $(this).toggleClass('active');
+      }
+    });
+    $(event.target).toggleClass('active');
+    event.preventDefault();
+  });
+
+})(jQuery);
