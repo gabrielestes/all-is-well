c46e1d793dd6f17e756bd845f273289dc7f9bb8c
Marian Powell <marianpowell@gmail.com>
2016-11-17 08:23:30 -0500

    fixed <a>edit</a> by nesting .disappear class in more specific context in the _profile.scss file.

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,7 +3,7 @@
   $(document).on('click','a.edit',function(event){
     $(event.target).toggleClass('active').parent().parent().next('aside').toggleClass('disappear');
     console.log($(event.target).parent().parent().next('aside'));
-    event.preventDefault();
+    // event.preventDefault();
   });
 
 
