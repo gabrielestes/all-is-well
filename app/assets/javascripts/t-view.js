f1a33809c030b77ab481d43bd70260a69da10c60
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-18 12:56:14 -0500

    built basic text area input for create note page

--- app/assets/javascripts/t-view.js
+++ app/assets/javascripts/t-view.js
@@ -1,10 +1,38 @@
 (function($) {
   "use strict";
+
+ var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+//Page elements that make up the therapist-view dashboard and sub-header.
+ var dashboardElements = [
+    '.t-view-dashboard',
+    '.t-view-header',
+    '.t-view-content',
+    '.t-view-dashboard-icon'
+  ];
+
+//Event handler to display or hide the therapist-view dashboard.
   $(document).on('click', '.t-view-dashboard-icon', function() {
-    $('.t-view-dashboard').toggleClass('t-dashboard-active');
-    $('.t-view-header').toggleClass('t-dashboard-active');
-    $('.t-view-content').toggleClass('t-dashboard-active');
-    $('.t-view-dashboard-icon').toggleClass('t-dashboard-active');
+     dashboardElements.forEach(function(element) {
+        $(element).toggleClass('t-dashboard-active');
+      });
+
+
+
+//****Below function makes the dashboard automatically open if the display width is bigger than 700px****
+//***Commented out because it also makes the dashboard always slide in on page load and that is weird****
+//****TODO: Figure out how to get it to display without sliding in on page load.****
+
+// $(document).on('ready', function() {
+//   if (width >= 700) {
+//     console.log(width);
+//     dashboardElements.forEach(function(element) {
+//       $(element).addClass('t-dashboard-active');
+//     });
+//   }
+// });
+
+
 
   });
 
