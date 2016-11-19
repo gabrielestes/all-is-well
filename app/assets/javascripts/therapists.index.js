00db32022403f74711b82512083324926542490d
Marian Powell <marianpowell@gmail.com>
2016-11-18 19:58:24 -0500

    local changes before pulling Jenn's file into my master.

--- app/assets/javascripts/therapists.index.js
+++ app/assets/javascripts/therapists.index.js
@@ -2,10 +2,7 @@
 "use strict";
   $(document).on('click', '.sort', function(event){
     $(event.target).parent('.sort').find('a').toggleClass('active');
-    event.preventDefault();
-    if( $(event.target).find('a').hasClass('active') ){
 
-    }
   });
 
 })(jQuery);
