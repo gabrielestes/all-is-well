aea50c977bd92b35a56007f9e4730327fcc36b70
Marian Powell <marianpowell@gmail.com>
2016-11-19 18:42:48 -0500

    working on the javascript for the entries page.

--- app/assets/javascripts/entries_index.js
+++ app/assets/javascripts/entries_index.js
@@ -1,27 +1,28 @@
 document.addEventListener("turbolinks:load", function(){
 "use strict";
 
-
   //on button click, show previous entries and shrink buttons
-  $(document).on('click','#view-previous-entries-btn',function(event){
-    if( $('.button-box').hasClass('shrink') ) {
-      $('.add_entry').addClass('disappear');
+  $(document).on('click','.button-box .button',function(event){
+    var pressed = '#' + $(event.target).attr("id");
+    var notPressed = '#' + $(event.target).siblings('.button').attr('id');
+
+    //if the pressed button's view is not hidden, hide both views and expand the buttons
+    if( ! ($('pressed').hasClass('disappear')) ) {
+      console.log('pressed does not have class disappear');
+      $('.entry-view').addClass('disappear');
+      $('.button-box').removeClass('shrink');
+
+    //if the pressed button's view is hidden, show that view only and shrink the buttons
     } else {
-      $('.previous_entries').removeClass('disappear');
+      console.log('pressed has class disappear');
+      $('pressed').removeClass('disappear');
       $('.button-box').addClass('shrink');
+    if( ! ($('notPressed').hasClass('disappear')) ){
+      $('notPressed').addClass('disappear');
+      }
     }
-  });
-  //on button click, show add entry page and shrink buttons
-  $(document).on('click','#add-new-entry-btn',function(event){
-    if( $('.button-box').hasClass('shrink')){
-      $('.previous_entries').addClass('disappear');
-    } else {
-      $('.add-entry').removeClass('disappear');
-      $('.button-box').toggleClass('shrink');
-    }
-  });
 
-
+    });
 
 
 
