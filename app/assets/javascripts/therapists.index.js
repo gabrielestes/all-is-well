c101a741a42025942b6d2c644021efd39970b1b9
Marian Powell <marianpowell@gmail.com>
2016-11-19 13:10:13 -0500

    dashboard working for client stuff.

--- app/assets/javascripts/therapists.index.js
+++ app/assets/javascripts/therapists.index.js
@@ -1,6 +1,23 @@
+
 (function($){
-"use strict";
-  $(document).on('click', '.sort', function(event){
-    $(event.target).parent('.sort').find('a').toggleClass('active');
+
+    //on page load, remove .actives from either title.
+    $('.sort a').removeClass('actives');
+    //check storage for existing activeItem
+    var storage = localStorage.getItem('activeItem');
+    //assign .actives to most recently chosen item based on what was stored in localStorage
+        var id = "#" + storage;
+        $(id).addClass('actives');
+        localStorage.setItem('activeItem',storage);
+  //on click, re-assign .actives to clicked element and store it in localStorage under that element
+  $(document).on('click', '.sort a', function(event){
+    //get id of clicked element and store in localStorage
+    var newStorage = $(event.target).attr("id");
+    localStorage.setItem('activeItem',newStorage);
+    //assign .actives to the clicked element to apply styling
+    $('#' + newStorage).addClass('actives');
+    console.log('#' + newStorage);
   });
-}(jQuery));
+
+
+})(jQuery);
