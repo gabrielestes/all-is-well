765b49e3325504afc8ed64136b947f7c18d9b456
Marian Powell <marianpowell@gmail.com>
2016-11-19 13:58:20 -0500

    open dashboard on 700+ page refresh

--- app/assets/javascripts/c-view.js
+++ app/assets/javascripts/c-view.js
@@ -2,7 +2,7 @@
   "use strict";
 
  var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
+console.log(width + "not in function");
 //Page elements that make up the therapist-view dashboard and sub-header.
  var dashboardElements = [
     '.c-view-dashboard',
@@ -12,6 +12,20 @@
     '.show-dash-text',
     '.hide-dash-text'
   ];
+  $(document).ready(function(){
+  if (width >= 700) {
+    console.log(width);
+    dashboardElements.forEach(function(element) {
+      $(element).addClass('c-dashboard-active');
+      console.log(element);
+    });
+  }
+});
+  //****Below function makes the dashboard automatically open if the display width is bigger than 700px****
+  //***Commented out because it also makes the dashboard always slide in on page load and that is weird****
+  //****TODO: Figure out how to get it to display without sliding in on page load.****
+
+
 
 //Event handler to display or hide the therapist-view dashboard.
   $(document).on('click', '.c-dashboard-toggle-container', function() {
@@ -21,18 +35,6 @@
 
 
 
-//****Below function makes the dashboard automatically open if the display width is bigger than 700px****
-//***Commented out because it also makes the dashboard always slide in on page load and that is weird****
-//****TODO: Figure out how to get it to display without sliding in on page load.****
-
-// $(document).on('ready', function() {
-//   if (width >= 700) {
-//     console.log(width);
-//     dashboardElements.forEach(function(element) {
-//       $(element).addClass('t-dashboard-active');
-//     });
-//   }
-// });
 
 
 
