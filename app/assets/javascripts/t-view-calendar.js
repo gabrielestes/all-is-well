0323d4f5cb2f3d82b18ffa21442ed8c3484dead5
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-19 23:44:27 -0500

    making good progress on getting event data from ajax request on calendar page

--- app/assets/javascripts/t-view-calendar.js
+++ app/assets/javascripts/t-view-calendar.js
@@ -2,7 +2,31 @@
 (function($) {
   "use strict";
 
+
   $(document).on('turbolinks:load', function() {
+
+    var path = location.pathname;
+    var pathArr = path.toString().split('/');
+    var clientId = pathArr.pop();
+
+    var clientEvents = [];
+
+
+    if (pathArr[1] === "therapist" && pathArr[2] === "calendar" ) {
+        console.log('on calendar page for client: ' + clientId);
+
+        $.ajax({
+          url: '/calendar/json/' + clientId,
+          method: 'GET',
+        }).done(function(data) {
+          clientEvents = data;
+          console.log(clientEvents);
+        });
+
+    //***TODO*** Loop through the events array and create a new array with objects that have properties that the calendar accepts. then pass to calendar to display******
+
+    }
+
 
 
       // page is now ready, initialize the calendar...
