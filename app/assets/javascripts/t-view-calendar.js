9b6ad0eab39635b89acd71ea42210537e715a808
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-19 23:57:33 -0500

    calendar displays events using ajax. display only, not clickable for more information yet. only displays “negative” or “positive” as the title currently.

--- app/assets/javascripts/t-view-calendar.js
+++ app/assets/javascripts/t-view-calendar.js
@@ -9,7 +9,8 @@
     var pathArr = path.toString().split('/');
     var clientId = pathArr.pop();
 
-    var clientEvents = [];
+    var displayEvents = [];
+
 
 
     if (pathArr[1] === "therapist" && pathArr[2] === "calendar" ) {
@@ -19,8 +20,31 @@
           url: '/calendar/json/' + clientId,
           method: 'GET',
         }).done(function(data) {
-          clientEvents = data;
-          console.log(clientEvents);
+          var clientEvents = [];
+          data.forEach(function(event) {
+            event.start = event.date;
+            event.title = event.event_type;
+            clientEvents.push(event);
+          }
+        );
+
+        displayEvents = clientEvents;
+        console.log(displayEvents);
+
+        $('#calendar').fullCalendar(
+          {
+            events: displayEvents,
+
+            eventClick: function(event) {
+               if (event.url) {
+                   window.open(event.url);
+                   return false;
+               }
+            }
+        });
+
+
+
         });
 
     //***TODO*** Loop through the events array and create a new array with objects that have properties that the calendar accepts. then pass to calendar to display******
@@ -31,40 +55,19 @@
 
       // page is now ready, initialize the calendar...
 
-      $('#calendar').fullCalendar({
-          // put your options and callbacks here
-
-    events: [
-        {
-            title  : 'event1',
-            start  : '2016-11-11',
-            url: 'http://www.google.com'
-        },
-        {
-            title  : 'event2',
-            start  : '2016-11-16',
-            url: 'http://www.amazon.com'
-        },
-        {
-            title  : 'event3',
-            start  : '2016-11-09T12:30:00',
-            allDay : false, // will make the time show
-            url: 'http://www.yahoo.com'
-        }
-    ],
-
-    eventClick: function(event) {
-       if (event.url) {
-           window.open(event.url);
-           return false;
-       }
-   }
+      // $('#calendar').fullCalendar(
+      //   {
+      //     events: displayEvents,
+      //
+      //     eventClick: function(event) {
+      //        if (event.url) {
+      //            window.open(event.url);
+      //            return false;
+      //        }
+      //     }
+      // });
 
 
-
-
-
-      });
 
 
 
