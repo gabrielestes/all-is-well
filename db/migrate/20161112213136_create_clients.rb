a24fa1a18b9453aac0271d3a487e703aa323a874
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-21 11:49:41 -0500

    Update client profile. Add read migration.

--- db/migrate/20161112213136_create_clients.rb
+++ db/migrate/20161112213136_create_clients.rb
@@ -5,7 +5,7 @@ class CreateClients < ActiveRecord::Migration[5.0]
       t.string :first_name
       t.string :last_name
       t.string :initial
-      t.string :birth_date
+      t.datetime :birth_date
       t.string :phone
       t.json :emergency
       t.integer :wellness
--- db/migrate/20161112214915_create_posts.rb
+++ db/migrate/20161112214915_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.belongs_to :client, index: true, foreign_key: true
       t.boolean :sharable, default: true
+      t.boolean :read, default: false
       t.string :title
       t.text :body
 
