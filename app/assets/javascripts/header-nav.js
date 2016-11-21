e4faa831c63fdcf2699e739db924549ac01c0e6f
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-21 17:30:29 -0500

    header links updates to reflected which header view is selected

--- app/assets/javascripts/header-nav.js
+++ app/assets/javascripts/header-nav.js
@@ -1,40 +1,46 @@
 (function($) {
   "use strict";
 
-$(document).on("turbolinks:load", function() {
+
+//Gets reads location URL and determines if header links should be styled appropriately if user is on home or account page.
+function getHeaderLocation() {
 
   var path = location.pathname;
   var pathArr = path.split('/');
-  console.log(pathArr);
 
-if (pathArr[1] === "therapist" &&  pathArr[2] === "profile" ) {
-    $('#home').removeClass('selected');
-    $('#account').addClass('selected');
+  if (pathArr[1] === "therapist" && pathArr.length === 2 || pathArr[1] === "client" && pathArr.length === 2 ) {
+    $('#nav-account').removeClass('selected');
+    $('#nav-home').addClass('selected');
+  }
+
+  if (pathArr[1] === "therapist" &&  pathArr[2] === "profile" || pathArr[1] === "client" &&  pathArr[2] === "profile" ) {
+      $('#nav-home').removeClass('selected');
+      $('#nav-account').addClass('selected');
+  }
+
 }
 
 
-if (pathArr[1] === "therapist" &&  Number.isInteger(parseFloat(pathArr[2])) === true ) {
-    $('#account').removeClass('selected');
-    $('#home').addClass('selected');
-}
+$(document).on("turbolinks:load", function() {
 
-
-
-
-
-
-
+//Gets page location to assign correct styling of header links on page load
+  getHeaderLocation();
 
 
 //Displays navigation menu at mobile widths on menu icon click
   $(document).on('click', '#header-nav-icon', function() {
     $('nav.header-nav').toggleClass('header-nav-active');
+
+
   });
-
-
-
 });
 
+
+
+$(document).on("turbolinks:render", function() {
+  //Gets page location to assign correct styling of header links on page render
+  getHeaderLocation();
+});
 
 
 
