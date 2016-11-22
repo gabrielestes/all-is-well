e300d51b593aafdf4562783493928a01433c9bb7
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-22 16:33:15 -0500

    added event source differentiation between event types and styling objects accordingly

--- app/assets/javascripts/t-view-calendar.js
+++ app/assets/javascripts/t-view-calendar.js
@@ -2,7 +2,6 @@
 (function($) {
   "use strict";
 
-
   $(document).on('turbolinks:load', function() {
 
     var path = location.pathname;
@@ -12,7 +11,7 @@
     var displayEvents = [];
 
 
-  //Checks the location to 
+  //Checks the page location before making GET request for event data
     if (pathArr[1] === "therapist" && pathArr[2] === "calendar" ) {
 
         $.ajax({
@@ -20,14 +19,37 @@
           method: 'GET',
         }).done(function(data) {
           var clientEvents = [];
+          var positiveEvents = [];
+          var negativeEvents = [];
+          //Loops through events and assigns properties that are needed to display properly once passed to calendar
           data.forEach(function(event) {
             event.start = event.date;
-            event.title = event.event_type;
-            clientEvents.push(event);
+            event.title = event.event_type.toUpperCase();
+            if (event.event_type === "negative") {
+              negativeEvents.push(event);
+            } else if (event.event_type === "positive") {
+              positiveEvents.push(event);
+            }
           }
         );
 
-        displayEvents = clientEvents;
+        var positiveColor = "#5EAAED";
+        var negativeColor = "#b1269e";
+
+        var positiveEventsObj = {
+          events: positiveEvents,
+          backgroundColor: positiveColor,
+          color: positiveColor
+        };
+
+        var negativeEventsObj = {
+          events: negativeEvents,
+          backgroundColor: negativeColor,
+          color: negativeColor
+        };
+
+
+
 
 
         $('#calendar').fullCalendar(
@@ -38,26 +60,29 @@
               right:  'month, basicWeek'
             },
 
-            events: displayEvents,
+            eventSources: [
+                positiveEventsObj,
+                negativeEventsObj
+            ],
 
-            eventClick: function(event) {
-               if (event.url) {
-                   window.open(event.url);
-                   return false;
-               }
-            }
+
+            eventClick: function(calEvent, jsEvent, view) {
+
+         alert('Event: ' + calEvent.title);
+         alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+         alert('View: ' + view.name);
+
+         // change the border color just for fun
+         $(this).css('border-color', 'red');
+
+     }
         });
 
 
 
         });
-
-
-    }
-
-
+     }
 
   });
-
 
 })(jQuery);
