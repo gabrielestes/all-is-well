5533cee72c360740f3444cdc7aeba906793d5d88
Marian Powell <marianpowell@gmail.com>
2016-11-22 12:32:02 -0500

    fixed javascript on client view profile edit

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,13 +1,12 @@
 (function($){
   "use strict";
   $(document).on('click','a.edit',function(event){
+    //add .active to the link and show the following aside.
+    $(event.target).toggleClass('active').parent().parent().next('aside').toggleClass('disappear');
 
-    $(event.target).toggleClass('active').parent().parent().next('aside').slideToggle();
-
+    //if the link is .active, make sure the display is set to flex.
     if ( $(event.target).hasClass('active') ) {
-      $(event.target).parent().parent().next('aside').css('display','flex');
-
+      $(event.target).parent().parent().next('aside');
     }
   });
-
 })(jQuery);
