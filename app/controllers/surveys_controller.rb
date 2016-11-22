e91e5b01a4145b5c3b95c5cc1e48b3833016dda6
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-22 14:42:41 -0500

    Create survey form

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,17 @@
 class SurveysController < ApplicationController
   def surveys_index
   end
+
+  def create
+    @survey = Survey.new survey_params
+    if @survey.save!
+      redirect_to surveys_index_path
+    end
+  end
+
+  private
+
+  def survey_params
+    params.permit(:client_id, :read, :total, :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9)
+  end
 end
