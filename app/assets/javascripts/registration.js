3ed57075e473a152da119715ae17ad0814f0de3d
Marian Powell <marianpowell@gmail.com>
2016-11-22 14:50:06 -0500

    changed around shareable icons and toggle. also made registration title update after registration type is chosen.

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -4,11 +4,14 @@
   $(document).on('click','.register-therapist',function(event){
     $('.register-therapist , .register-client').hide();
     $('.new-therapist').addClass('active');
+    $('.welcome, h1').html('Register a Therapist');
   });
 
   $(document).on('click','.register-client',function(event){
     $('.register-therapist , .register-client').hide();
     $('.new-client').addClass('active');
+    $('.welcome, h1').html('Register a Client');
+
   });
 
 
