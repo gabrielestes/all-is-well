1c7a6febd8da358bc56fb140e620eb7948f63056
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-22 12:25:26 -0500

    Add questions migration file

--- db/migrate/20161112215158_create_surveys.rb
+++ db/migrate/20161112215158_create_surveys.rb
@@ -3,16 +3,6 @@ class CreateSurveys < ActiveRecord::Migration[5.0]
     create_table :surveys do |t|
       t.belongs_to :client, index: true, foreign_key: true
       t.boolean :read, default: false
-      add_column :surveys, :total, :integer
-      add_column :surveys, :q1, :integer
-      add_column :surveys, :q2, :integer
-      add_column :surveys, :q3, :integer
-      add_column :surveys, :q4, :integer
-      add_column :surveys, :q5, :integer
-      add_column :surveys, :q6, :integer
-      add_column :surveys, :q7, :integer
-      add_column :surveys, :q8, :integer
-      add_column :surveys, :q9, :integer
 
       t.timestamps
     end
