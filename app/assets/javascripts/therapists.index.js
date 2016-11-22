6e45db2d0bd110fad05cb05ba939dbcdd0d49748
Marian Powell <marianpowell@gmail.com>
2016-11-22 15:10:57 -0500

    changed sort function to url-based method instead of local storage.

--- app/assets/javascripts/therapists.index.js
+++ app/assets/javascripts/therapists.index.js
@@ -2,25 +2,11 @@
 
 document.addEventListener("turbolinks:load", function(){
 
-    //on page load, remove .actives from either title.
+  var path = window.location.search;
     $('.sort a').removeClass('actives');
-
-    //check storage for existing activeItem
-    var storage = localStorage.getItem('activeItem') || "wellness";
-
-    //assign .actives to most recently chosen item based on what was stored in localStorage
-        var id = "#" + storage;
-        $(id).addClass('actives');
-        localStorage.setItem('activeItem',storage);
-  //on click, re-assign .actives to clicked element and store it in localStorage under that element
-  $(document).on('click', '.sort a', function(event){
-    //get id of clicked element and store in localStorage
-    var newStorage = $(event.target).attr("id");
-    localStorage.setItem('activeItem',newStorage);
-    //assign .actives to the clicked element to apply styling
-    $('#' + newStorage).addClass('actives');
-    console.log('#' + newStorage);
-  });
-
-
+  if ( path.includes('?sort_by=last_name')){
+    $('#name').addClass('actives');
+  } else {
+    $('#wellness').addClass('actives');
+  }
 });
