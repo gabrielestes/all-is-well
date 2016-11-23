d13cc5610ed7627351f17780573dac3fd174c6d3
Marian Powell <marianpowell@gmail.com>
2016-11-23 11:20:58 -0500

    fixed client 'submit' button so that it reloads the page.

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,6 +1,12 @@
 (function($){
   "use strict";
   $(document).on('click','a.edit',function(event){
+
+    //assign click event to 'Submit' button to refresh page
+    $('.button').on('click',function(){
+      location = location.href;
+    });
+
     //add .active to the link and show the following aside.
     $(event.target).toggleClass('active').parent().parent().next('aside').toggleClass('disappear');
 
