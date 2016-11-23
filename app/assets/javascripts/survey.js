7f36963626991eb5f03b50912751704a1f89e927
Marian Powell <marianpowell@gmail.com>
2016-11-23 09:24:57 -0500

    mood survey transitioning through questions properly. needs more styling and fix div that shows up at end.

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -11,19 +11,26 @@ document.addEventListener("turbolinks:load", function(){
     var current = 0;
   $('#next-question').on('click',function(){
     current++;
+    console.log(current);
     $('#next-question').html("Next").show();
     if(current >= 1){
       $('#q' + current).addClass('openQ').removeClass('closeQ');
     }
     if(current > 1){
       $('#q' + (current - 1)).addClass('closeQ').removeClass('openQ');
+      $('#last-question').show();
     }
     if(current === numQ){
+      $('.button').show();
       $('#next-question').hide();
+    }
+    if(current < numQ){
+      $('.button').hide();
     }
   });
   $('#last-question').on('click',function(){
     current--;
+    console.log(current);
     if(current <= 1){
       $('#last-question').hide();
     }
@@ -36,6 +43,9 @@ document.addEventListener("turbolinks:load", function(){
     }
     if(current === numQ-1){
       $('#next-question').show();
+    }
+    if(current < numQ){
+      $('.button').hide();
     }
   });
   $('#new_survey').find('.submit').on('click',function(){
