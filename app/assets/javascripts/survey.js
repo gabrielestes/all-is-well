fa5291ec920706527a9baff03f6a6047bbb23835
Marian Powell <marianpowell@gmail.com>
2016-11-23 00:22:22 -0500

    questions more or less working but styling a nightmare still.

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -3,45 +3,46 @@
 
 document.addEventListener("turbolinks:load", function(){
 
-//hide all questions
-$('#new_survey').find('.question').hide();
-
-//on click, begin survey
-$('#next-question').on('click',function(){
-  //hide #begin-survey, show questions
-  $('#next-question').html("Next");
-  var numQ = 9;
-  var current = 1;
-
-  //add click events to progression buttons
+  //hide all questions, 'submit' button ,and 'previous' button
+  $('#new_survey').find('.question').addClass('closeQ');
+  $('#last-question').hide();
+  $('#new_survey').find('.button').hide();
+    var numQ = 9;
+    var current = 0;
   $('#next-question').on('click',function(){
-      current++;
-      //show next question, hide previous question
-      $('#q' + current).slideToggle();
-      if(current > 1){
-        $('#q' + (current - 1)).slideToggle();
-      }
-      if(current === numQ){
-        $('#next-question').hide();
-      }
+    current++;
+    $('#next-question').html("Next").show();
+    if(current >= 1){
+      $('#q' + current).addClass('openQ').removeClass('closeQ');
+    }
+    if(current > 1){
+      $('#q' + (current - 1)).addClass('closeQ').removeClass('openQ');
+    }
+    if(current === numQ){
+      $('#next-question').hide();
+    }
   });
   $('#last-question').on('click',function(){
-      current--;
-      // show last question, hide current question
-      current++;
-      //show next question, hide previous question
-      $('#q' + current).slideToggle();
-      if(current > 1){
-        $('#q' + (current - 1)).slideToggle();
-      }
-      if(current === numQ){
-        $('#next-question').hide();
-      }
+    current--;
+    if(current <= 1){
+      $('#last-question').hide();
+    }
+    if(current > 1){
+      $('#last-question').show();
+      $('#q' + (current + 1)).addClass('closeQ').removeClass('openQ');
+    }
+    if(current >= 1){
+      $('#q' + current).addClass('openQ').removeClass('closeQ');
+    }
+    if(current === numQ-1){
+      $('#next-question').show();
+    }
+  });
+  $('#new_survey').find('.submit').on('click',function(){
+    $('main.survey').append('h2').html('Thank you for submitting a survey!');
   });
 
 });
 
-
-});//end of turbolinks event listener
 
 })(jQuery);
