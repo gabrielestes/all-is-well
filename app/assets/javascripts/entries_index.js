8fd0c9cd1645ba71de5b328e2744574b9fa4500a
Marian Powell <marianpowell@gmail.com>
2016-11-23 21:10:02 -0500

    made entries show small buttons and previous entries first thing.

--- app/assets/javascripts/entries_index.js
+++ app/assets/javascripts/entries_index.js
@@ -8,22 +8,20 @@
     //remove '-btn' from the end of the button id and use that to target the view id
     var selected = '#' + $(event.target).attr("id");
     selected = selected.substring(0, selected.indexOf('-btn'));
-    console.log(selected);
     var notSelected = '#' + $(event.target).siblings('.button').attr('id');
     notSelected = notSelected.substring(0, notSelected.indexOf('-btn'));
-    console.log(notSelected);
 
     //if the pressed button's view is hidden, show that view only and shrink the buttons
     if ( $(selected).hasClass('disappear') ) {
 
       $(selected).removeClass('disappear');
       $(notSelected).addClass('disappear');
-      $('.button-box').addClass('shrink');
+      // $('.button-box').addClass('shrink');
 
     //if the pressed button's view is not hidden, hide both views and re-expand the buttons
     } else {
       $('.entry-view').addClass('disappear');
-      $('.button-box').removeClass('shrink');
+      // $('.button-box').removeClass('shrink');
     }
 
   });
