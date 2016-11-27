32dbb8056cc70b6047a2a7f3f41f1497a1ecee3e
Marian Powell <marianpowell@gmail.com>
2016-11-26 22:38:51 -0500

    survey- can check buttons by clicking labels

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -9,19 +9,21 @@ document.addEventListener("turbolinks:load", function(){
   $('#new_survey').find('.button').hide();
     var numQ = 9;
     var current = 0;
+
+  $('.choices').on('click','label', function(event){
+    $(event.target).prev().attr('checked','true');
+  });
+
   $('#next-question').on('click',function(){
-<<<<<<< HEAD
-      if (current === 0) {
-        $("main.survey").css('padding','0');
-      }
-=======
->>>>>>> master
+
+
           if (current > 0) {
           var id = "q" + current;
           if (!$('input[name=' + id + ']:checked').length) {
             return;
+            }
           }
-        }
+
 
         $('#new_survey .button').on('click',function(){
           var id = "q" + current;
@@ -42,15 +44,16 @@ document.addEventListener("turbolinks:load", function(){
           $('#q' + (current - 1)).addClass('closeQ').removeClass('openQ');
           $('#last-question').show();
         }
-        if(current === numQ){
-          $('.button').show();
-          $('#next-question').hide();
-        }
+        $('input[type=radio]').on('click',function(){
+          if(current === numQ && $('input[name=q9]:checked').length > 0 ){
+            $('.button').show();
+            $('#next-question').hide();
+          }
+        });
         if(current < numQ){
           $('.button').hide();
         }
-
-  });
+      });
 
   $('#last-question').on('click',function(){
     current--;
@@ -70,6 +73,7 @@ document.addEventListener("turbolinks:load", function(){
     if(current < numQ){
       $('.button').hide();
     }
+
   });
 
 });
