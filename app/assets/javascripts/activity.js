384aa970b9992c03411b96df35dfd3c596a27d5f
donaldsonjulia <julia.donaldson@gmail.com>
2016-11-28 13:59:03 -0500

    fixed unread alert styling and activity.js bugs

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -39,12 +39,11 @@
         $(card).removeClass('active');
       });
 
-  //
+  // Gets the data ID information from the clicked card
     var activityId = $(clickedCard).attr('data-id');
     var activityType = $(clickedCard).attr('data-activity-type');
-    console.log(activityId);
-    console.log(activityType);
 
+  // Updates the database entry using data ID to mark activity as "READ"
     $.ajax({
         url: '/therapist/update_read',
         method: 'PUT',
@@ -54,12 +53,10 @@
                 },
         success:
                 function(){
-                    console.log('SUCCESS!');
                     $(clickedCard).addClass('read');
-                
                 },
         failure:
-                function(){ console.log('FAILS');
+                function(error){ console.log(error);
                 }
     });
 
@@ -71,12 +68,12 @@
           var negativeColor = "#4c5fce";
           var neutralColor = "#928EA0";
         // Gets clicked event data
-          var thisEventDate = $(clickedCard).find('.time').text();
-          var thisEventType = $(clickedCard).find('.type-of').text().toUpperCase();
-          var thisEventDescription = $(clickedCard).find('.event-content').text();
+          var thisEventDate = $.trim($(clickedCard).find('.time').text());
+          var thisEventType = $.trim($(clickedCard).find('.type-of').text().toUpperCase());
+          var thisEventDescription = $.trim($(clickedCard).find('.event-content').text());
 
 
-
+          console.log(thisEventType);
         // Updates event detail card with the clicked event data
           $('#event-date').text(thisEventDate);
           $('#event-type').text(thisEventType);
@@ -85,13 +82,13 @@
 
           // Changes background color of subheader to match the event-type
             switch(thisEventType) {
-              case ' POSITIVE ':
+              case 'POSITIVE':
                   $('.event-type-sub-header').css('background-color', positiveColor);
                   break;
-              case ' NEGATIVE ':
+              case 'NEGATIVE':
                     $('.event-type-sub-header').css('background-color', negativeColor);
                   break;
-              case ' NEUTRAL ':
+              case 'NEUTRAL':
                     $('.event-type-sub-header').css('background-color', neutralColor);
                   break;
             }
@@ -105,9 +102,9 @@
   /* *********************************************** */
     if ($(clickedCard).hasClass('entry')) {
         // Gets clicked entry data
-          var thisEntryDate = $(clickedCard).find('.time').text();
-          var thisEntryTitle = $(clickedCard).find('.entry-title').text().toUpperCase();
-          var thisEntryDescription = $(clickedCard).find('.entry-content').text();
+          var thisEntryDate = $.trim($(clickedCard).find('.time').text());
+          var thisEntryTitle = $.trim($(clickedCard).find('.entry-title').text().toUpperCase());
+          var thisEntryDescription = $.trim($(clickedCard).find('.entry-content').text());
 
         // Updates entry detail card with the clicked entry data
           $('#entry-date').text(thisEntryDate);
@@ -118,23 +115,25 @@
           $('.entry-detail-card').addClass('active');
         }
 
-//Gets survey results from hidden elements using JQuery
+/* ****** below functions for use if card is MOOD SURVEY CARD ******* */
+/* ****************************************************************** */
+  //Gets survey results from hidden elements using JQuery
   function getSurveyResults(clickedCard) {
     var answersArr = [];
     for (index = 1; index <= 9; index++) {
       var classname = '.q' + index;
-      var answer = $(clickedCard).find(classname).text();
+      var answer = $.trim($(clickedCard).find(classname).text());
       switch(answer) {
-        case ' 0 ':
+        case '0':
           answer = "Not At All (0)";
           break;
-        case ' 1 ':
+        case '1':
           answer = "Several Days (1)";
           break;
-        case ' 2 ':
+        case '2':
           answer = "More Than Half the Days (2)";
           break;
-        case ' 3 ':
+        case '3':
           answer = "More Than Every Day (3)";
           break;
       }
@@ -143,10 +142,10 @@
 
     return answersArr;
   }
-
+  //Replaces detail card text with answers from clicked card
   function setAnswers(answersArr) {
     for (index = 0; index < answersArr.length; index++) {
-  
+
       var question = '#q' + (index + 1);
       $(question).html(answersArr[index]);
     }
@@ -157,16 +156,14 @@
   /* *********************************************** */
     if ($(clickedCard).hasClass('mood')) {
         // Gets clicked mood survey data
-          var thisSurveyDate = $(clickedCard).find('.time').text();
-          var thisSurveyScore = $(clickedCard).find('.score').text();
-          var thisSurveyResults = $(clickedCard).find('.q2').text();
+          var thisSurveyDate = $.trim($(clickedCard).find('.time').text());
+          var thisSurveyScore = $.trim($(clickedCard).find('.score').text());
           var thisMoodIcon = $(clickedCard).find('.mood-icon').clone();
 
 
         // Updates event detail card with the clicked event data
           $('#mood-date').text(thisSurveyDate);
           $('#mood-score').text('MOOD SCORE: ' + thisSurveyScore);
-          $('#mood-survey').text('Question 2 Answer ' + thisSurveyResults);
           $('#mood-icon').html(thisMoodIcon);
 
           var thisAnswers = getSurveyResults(clickedCard);
