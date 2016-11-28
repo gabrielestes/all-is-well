1b8b5d0c77872783573cb638c14b7a7de8122388
Gabriel <gabriel.estes@gmail.com>
2016-11-28 15:22:45 -0500

    User is invalid without email, pw, and pw confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,45 @@
 require 'rails_helper'
 
+
 RSpec.describe User, :type => :model do
+
+  subject { User.new }
+
+  therapist = Therapist.create(first_name: 'John', last_name: 'Smith', cred: 'LCSW', phone: '9191234567')
+
   it "is valid with valid attributes" do
-    t = Therapist.create(first_name: 'John', last_name: 'Smith', cred: 'LCSW', phone: '9191234567')
-    expect(User.new(
-                    email: 'name@example.com',
-                    password: 'password',
-                    password_confirmation: 'password',
-                    userable: t
-                    )).to be_valid
+    subject.email = 'name@example.com'
+    subject.password = 'password'
+    subject.password_confirmation = 'password'
+    subject.userable = therapist
+    expect(subject).to be_valid
   end
 
-  it "is invalid without an email"
-  it "is invalid without a password"
-  it "is invalid without a userable (client or therapist)"
+  it "is invalid without an email" do
+    subject.password = 'password'
+    subject.password_confirmation = 'password'
+    subject.userable = therapist
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a password" do
+    subject.email = 'name@example.com'
+    subject.password_confirmation = 'password'
+    subject.userable = therapist
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a password_confirmation" do
+    subject.email = 'name@example.com'
+    subject.password = 'password'
+    subject.userable = therapist
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a userable (client or therapist)" do
+    subject.email = 'name@example.com'
+    subject.password = 'password'
+    subject.password_confirmation = 'password'
+    expect(subject).to_not be_valid
+  end
 end
