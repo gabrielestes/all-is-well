172e844899623ed7ee31c2d13ff365481ecb1139
Gabriel <gabriel.estes@gmail.com>
2016-11-28 19:45:50 -0500

    Add error message to event page. Write tests and pass for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,29 @@
 class EventsController < ApplicationController
   def events_index
-    @types = ["good", "not so good", "neither good nor bad"]
+  end
+
+  def new
+    @event = Event.new event_params
   end
 
   def create
     client_user
-    event = Event.new event_params
-    event.client_id = @current_client.id
-    event.event_type = params[:event_type]
-    if event.event_type == 'good'
-      event.event_type = 'positive'
-    elsif event.event_type == 'not so good'
-      event.event_type = 'negative'
-    elsif event.event_type == 'neither good nor bad'
-      event.event_type = 'neutral'
+    @event = Event.new event_params
+    @event.client_id = @current_client.id
+    @event.event_type = params[:event_type]
+    if @event.event_type == 'good'
+      @event.event_type = 'positive'
+    elsif @event.event_type == 'not so good'
+      @event.event_type = 'negative'
+    elsif @event.event_type == 'neither good nor bad'
+      @event.event_type = 'neutral'
     end
 
-    if event.save!
+    if @event.save
       redirect_to event_complete_path
+    else
+      flash[:danger] = "*All fields must be completed to submit an event."
+      render :action=>'events_index'
     end
   end
 
