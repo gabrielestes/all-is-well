23247557604eb250c4b235c91c4767316f5ee9ce
Jenn Tustin <jenn.tustin@gmail.com>
2016-11-30 15:35:26 -0500

    changed files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -23,7 +24,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
+  #THIS IS WHAT I JUST CHANGE
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
